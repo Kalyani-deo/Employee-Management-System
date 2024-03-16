d514e484dc2c771c440f6cfb5f3cbabb518ff035
Kalyani-deo <kalyanideore29@gmail.com>
2024-03-16 21:32:53 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/EmpController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmpService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class EmpController {
+	
+	@Autowired
+	private EmpService service;
+
+	@GetMapping("/")
+	public String home()
+	{
+		return "index";
+	}
+	@GetMapping("/addemp")
+	public String addEmpForm()
+	{
+		return "add_emp";
+	}
+	@PostMapping("/register")
+	public String empRegister(@ModelAttribute Employee e)
+	{
+		
+		service.addEmp(e);
+		
+		return "redirect:/";
+	}
+	
+}
